6d707e7255dd9129ca5a21bffcc9e875053e33f5
Kaushal <mehrakaushal54@gmail.com>
2026-09-12 11:08:16 +0530

    basic

---
+++ Learning SQL for AI-ML/Lecture1 query 2026-09-12 10_56_16.sql
@@ -0,0 +1,89 @@
+-- select *
+-- from `e1.categories`
+
+
+--Show me the top 10 records of the customers 
+select * 
+from `e1.customers` 
+limit 10
+
+what is data? 
+A. A programming language
+B. A collection of facts
+C. A database
+D. A chart
+Ans: B  
+Which of the following is an example of data? 
+A. Rahul Sharma
+B. ₹15,000
+C. Nagpur
+D. All of the above
+Ans D      
+A database is: 
+
+A. A collection of songs
+B. An organized collection of data
+C. A calculator
+D. A web browser
+Ans B 
+Which of the following companies uses data extensively? 
+A. Amazon
+B. Swiggy
+C. Netflix
+D. All of the above
+Ans: D 
+What is the primary purpose of a database? 
+A. Watching videos
+B. Storing and managing data
+C. Creating presentations
+D. Playing games
+Ans: B 
+DBMS --> Database management system 
+In the NextKart database, which table stores customer information? 
+A. products
+B. orders
+C. customers
+D. payments
+Ans: C 
+Which table stores product details? 
+A. customers
+B. products
+C. reviews
+D. inventory
+Ans: B  
+What is a table? 
+A. A collection of similar records
+B. A programming language. A chart
+D. A report
+
+Ans: A  
+Which column uniquely identifies a customer? 
+A. customer_name
+B. city
+C. customer_id
+D. membership_type
+Ans: C 
+SQL stands for: 
+A. Standard Query Logic
+B. Structured Query Language
+C. Simple Question Language
+D. Structured Question Logic
+Ans : B 
+Which platform are we using in this course? 
+A. MySQL
+B. Oracle
+C. Google BigQuery
+D. MongoDB
+Ans: C 
+Google BigQuery is: 
+A. A spreadsheet software
+B. A cloud-based data warehouse
+C. A photo editor
+D. A programming language
+Ans: B 
+Which BigQuery tab lets you view data without writing SQL? 
+A. Schema
+B. Preview
+C. Results
+D. Explorer
+Ans: B
